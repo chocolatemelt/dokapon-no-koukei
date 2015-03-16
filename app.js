900c8a0fde6950fbe6cc37dcc20b7b939ef2ab63
Toshi Piazza <toshi.piazza@gmail.com>
2015-03-16 16:05:43 -0400

    Adds separate route for chat.js for better infrastructure

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var chat   = require('./routes/chat');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
