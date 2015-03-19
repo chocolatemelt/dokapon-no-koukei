0209bf76708d410720e5ae020253ce077ba4cab0
Kevin Zhang <zhangk6@rpi.edu>
2015-03-19 07:05:13 -0400

    Fixed chat demo

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,10 @@
 var socket = io();
-$('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+$(document).ready(function(){
+    $('form').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
 });
 socket.on('chat message', function(msg){
     $('#messages').append($('<li>').text(msg));
