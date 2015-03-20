17b645e42bdb7bc8812c73c02eab95685040aee0
Kevin Zhang <zhangk6@rpi.edu>
2015-03-19 20:46:14 -0400

    Changes socket to broadcast

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,7 @@
 var socket = io();
 $(document).ready(function(){
     $('form').submit(function(){
-        socket.emit('chat message', $('#m').val());
+        socket.broadcast.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
     });
