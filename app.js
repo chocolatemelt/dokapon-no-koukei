0c373e5fee1f82df73c8baa46f83d7fa3a2c6564
Toshi Piazza <toshi.piazza@gmail.com>
2015-03-24 19:50:30 -0400

    Adds support for /login url and integrates passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
+var login  = require('./routes/login');
 var chat   = require('./routes/chat');
 
 var app = express();
@@ -26,14 +27,14 @@ app.use(passport.initialize());
 //  not allow on the github repo
 passport.use(new LocalStrategy(
   function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
       return done(null, user);
     });
   }
@@ -57,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/chat', chat);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+
+/* login page */
+router.get('/', function (req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* login credentials sent through post */
+router.post('/',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+module.exports = router;
