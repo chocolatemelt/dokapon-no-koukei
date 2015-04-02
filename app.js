d757895bfad09fae938837ac1dfc6d3101341a09
Toshi Piazza <toshi.piazza@gmail.com>
2015-04-01 21:42:59 -0400

    Makes formatting consistent, edits css.
    - Makes indentation two spaces
    - Puts specifier on form to allow both login and chat form coexist

--- app.js
+++ app.js
@@ -99,16 +99,16 @@ app.use(function(err, req, res, next) {
 
 // socket stuff - hopefully we can put this in a separate file
 io.on('connection', function(socket) {
-      console.log('a user connected');
-      io.emit('chat message', 'a user connected');
-      socket.on('disconnect', function() {
-          console.log('a user disconnected');
-          io.emit('chat message', 'a user disconnected');
-      });
-      socket.on('chat message', function(msg) {
-          console.log('message: ' + msg);
-          io.emit('chat message', msg);
-      });
+  console.log('a user connected');
+  io.emit('chat message', 'a user connected');
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+    io.emit('chat message', 'a user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
 module.exports = app;
